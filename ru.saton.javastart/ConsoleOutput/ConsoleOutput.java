6c1ec1e430511089ee652dc5764eccda8aedce5a
Saton <CoolHippo@yandex.ru>
2016-05-18 21:25:40 +0500

    Create ConsoleOutput

---
+++ ru.saton.javastart/ConsoleOutput/ConsoleOutput.java
@@ -0,0 +1,12 @@
+package ConsoleOutput;
+
+/**
+ * Created by Saton on 18.05.2016.
+ */
+public class ConsoleOutput {
+    public static void main(String[] args) {
+        for (int i = 0; i <=1000; i++) {
+            System.out.print(i+" ");
+        }
+    }
+}
